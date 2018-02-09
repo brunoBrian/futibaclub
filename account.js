8d80ca5af312196809433dcbe40567ca044c9a47
Bruno Sousa <brunosousa@HDU15539.local>
2018-02-09 15:41:24 -0200

    configuring access and create login pages

--- account.js
+++ account.js
@@ -8,9 +8,60 @@ const init = connection => {
 
 		res.render('home')
 	})
+
+	app.get('/logout', (req, res) => {
+		req.session.destroy( err => {
+			res.redirect('/')
+		})
+	})
+
+	app.get('/login', (req, res) => {
+		res.render('login', {error: false} )
+	})
+
+	app.post('/login', async(req, res) => {
+		const [rows, field] = await connection.execute('select * from users where email = ?', [req.body.email])
+
+		if(rows.length === 0) {
+			res.render('login', { error: 'Usuário e/ou senha Inválidos'} )
+		} else {
+			if(rows[0].passwd === req.body.passwd) {
+				const userDb = rows[0]
+				const user = {
+					id: userDb.id,
+					name: userDb.name,
+					role: userDb.role
+				}
+				req.session.user = user
+				res.redirect('/')
+			}else {
+				res.render('login', { error: 'Usuário e/ou senha Inválidos'})
+			}
+		}
+	})
+
+	app.get('/new-account', (req, res) => {
+		res.render('new-account', {error: false} )
+	})
+
+	app.post('/new-account', async(req, res) => {
+		const [rows, fields] = await connection.execute('select * from users where email = ?', [req.body.email])
+		if(rows.length === 0) {
+			const {name, email, passwd} = req.body
+			await connection.execute('insert into users (name, email, passwd, role) values (?,?,?,?)', [
+				name, email, passwd, 'user'
+			])			
+			res.redirect('/')
+		}else {
+			res.render('new-account', {
+				'error' : 'Usuário já existente'
+			})
+			console.log('Deu merda')
+		}
+		
+	})
+
 	return app
 }
-
-
 
 module.exports = init
--- index.js
+++ index.js
@@ -1,10 +1,17 @@
 const express = require('express')
 const app = express('app')
 const mysql = require('mysql2/promise')
-
+const bodyParser = require('body-parser')
+const session = require('express-session')
 const account = require('./account')
 
 app.use(express.static('public'))
+app.use(bodyParser.urlencoded({ extended: true}))
+app.use(session({
+	secret: 'fullstack',
+	resave: true,
+	saveUninitialized : true
+}))
 app.set('view engine', 'ejs')
 
 const init = async() => {
@@ -13,6 +20,16 @@ const init = async() => {
 		user: 'root',
 		password: 'Cabelera10',
 		database: 'futibaclub'
+	})
+
+	app.use((req, res, next) => {
+		if(req.session.user) {
+			res.locals.user = req.session.user
+		}else {
+			res.locals.user = false
+		}
+		console.log(req.session)
+		next()
 	})
 
 	app.use(account(connection))
